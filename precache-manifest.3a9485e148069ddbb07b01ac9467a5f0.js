549ffbec3da7a4a1791eae55193b8e593d765a80
GiftedGeek <rajeshkumar19nintyeight@gmail.com>
2020-06-10 20:15:38 +0530

    Updates

---
+++ precache-manifest.3a9485e148069ddbb07b01ac9467a5f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca5110b18d4953bdbd955a0b4699eb2b",
+    "url": "/react-pagination/index.html"
+  },
+  {
+    "revision": "15c910f9b579e9ade7c0",
+    "url": "/react-pagination/static/css/main.1b92fdc8.chunk.css"
+  },
+  {
+    "revision": "d3af404b27f842642260",
+    "url": "/react-pagination/static/js/2.aea8cb5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pagination/static/js/2.aea8cb5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c910f9b579e9ade7c0",
+    "url": "/react-pagination/static/js/main.74856386.chunk.js"
+  },
+  {
+    "revision": "5a1d36824d24584d6102",
+    "url": "/react-pagination/static/js/runtime-main.979d10a1.js"
+  }
+]);
